d920b5dc5718a7aae21d5de123200912befb8de7
Nikhil <nickpaulzagde@gmail.com>
2022-12-01 10:01:21 +0530

    Creating serializers for the POST request

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+
+from rest_framework.response import Response
+from profiles_api import serializers
+
+class HelloApiview(APIView):
+
+    def get(self, request, format=None):
+
+        an_api = [
+
+        "Uses Statndard HTTP methods as function(get, post,put,delete)",
+
+        "It is similar to traditional Django view",
+
+        "Gives you control over ur application and logic",
+
+        "It mapped manually to URL's"
+
+        ]
+
+
+        return Response({"message":"hello world", "an_api" : an_api})
+
+class HelloApiview(APIView):
+    serializer_class=serializers.Helloserializer
+
+    def post(self, request):
+
+        serializer = self.serializer_class(data=request.data)
+
+        print("Log for serializer", type(serializer))
+
+        if serializer.is_valid():
+
+            name=serializer.validated_data.get("name")
+
+            age=serializer.validated_data.get("age")
+
+            message = f"hello {name, age}"
+
+            return Response({'message' : message})
+
+        else:
+
+            return Response(serializer.errors,
+
+            status = status.HTTP_400_BAD_REQUEST)
+
+    def put (self,respect , pk=None):
+        return Response({'method':'put'})
+
+    def patch(self,request,pk=None):
+        return Response({'method':'patch'})
+
+    def delete(self,request,pk=None):
+        return Response({'method':'delete'})        
